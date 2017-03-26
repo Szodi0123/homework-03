331d50e4224df4c2065b89decc39334efe5fd819
Szodi0123 <adam.szilagyi.23@gmail.com>
2017-03-26 23:32:51 +0200

    Elvileg kész van minden, jöhet a feltöltés

--- src/homework-03-functions.R
+++ src/homework-03-functions.R
@@ -11,8 +11,30 @@ vector_pls <- function(x){
   (runif(vector(length = sample(10:20,1))))
 }
 
-
+##Normalizáló függvény a VI.2-höz.
 be_normal_pls <- function(x){
   -1+2*(x-min(x))/
     (max(x)-min(x))
 }
+
+##Névkezelő függvény V.2-höz
+citizen_mode <- function(x){
+  sapply(strsplit(comic_characters$name, split = " (", fixed = T), "[",1)
+}
+
+##Get_gender függvény V.3-hoz
+get_gender <- function(name) {
+  if (is.na(comic_characters$gsm[comic_characters$name == name]) == TRUE) 
+  {
+    print(as.vector(substr(comic_characters$sex[comic_characters$name == name]
+                  ,1,
+                 nchar(comic_characters$sex[comic_characters$name == name])
+                 - 11)))
+  }else{
+    print(as.vector(comic_characters$gsm[comic_characters$name == name]))}
+ }
+
+
+
+
+
--- src/homework-03.R
+++ src/homework-03.R
@@ -8,28 +8,25 @@
 
 
 ##II.feladat
-  #II.1 feladat: Készíts 5 vektort a megadott feltételekkel.
-
-    #A feladatot egy vektorgeneráló függvénnyel végeztem el.
+#II.1 feladat: Készíts 5 vektort a megadott feltételekkel.
 source("src/homework-03-functions.R")
 
-    #Létrehozom az üres listát, majd feltöltöm:
+  #Létrehozom az üres listát, majd feltöltöm:
 my_vectors <- list()[1:5]
-my_vectors <- sapply(my_vectors,vector_pls)
+my_vectors <- sapply(my_vectors, vector_pls)
+print(my_vectors)
 
-  #II.2 feladat: Listahossz (for), végeredmény: vektor
+#II.2 feladat: Listahossz (for), végeredmény: vektor
 vec_len_for <- c()
 
 for (i in 1:length(my_vectors)) {
-vec_len_for[i] <- lengths(my_vectors[i])
+  vec_len_for[i] <- lengths(my_vectors[i])
 }
 
-  #II.3 feladat: Listahossz (apply), végeredmény: lista
-
+#II.3 feladat: Listahossz (apply), végeredmény: lista
 vec_len_app_l <- (sapply(my_vectors, length, simplify = F))
 
-  #II.4 feladat: Listahossz (apply), végeredmény: vektor
-
+#II.4 feladat: Listahossz (apply), végeredmény: vektor
 vec_len_app_v <- (sapply(my_vectors, length, simplify = T))
 
 class(vec_len_app_v)
@@ -37,51 +34,72 @@ class(vec_len_app_l)
 
 
 #III.feladat
-  #III.1 feladat: dataset behívása
-
+#III.1 feladat: dataset behívása
 data("chickwts")
 head(chickwts)
 
-  #III.2 feladat: dataset behívása
-chic_w_b_f<- (aggregate(chickwts$weight, list(chickwts$feed),
-                        FUN = mean, simplify = T))
+#III.2 feladat: dataset behívása
+chic_w_b_f <- (aggregate(
+  chickwts$weight,
+  list(chickwts$feed),
+  FUN = mean,
+  simplify = T
+))
 
 
 #III.3 feladat: csökkenő sorrend (a plyr csomagot használtam: átláthatóbb)
+install.packages("plyr", repos = "http://cran.r-project.org")
 require(plyr)
-arrange(chic_w_b_f, -chic_w_b_f$x)
+arrange(chic_w_b_f,-chic_w_b_f$x)
 
 #IV. feladat
-    #IV.1 feladat 50*10-es mátrix + normálelo. +szóráskritérium
-probamatrix <- matrix(1:50, 50,11)
-probamatrix[,1:10] <- (matrix(rnorm(500, mean = 0, sd =
-                                      probamatrix[,11]),50,10))
-probamatrix <- probamatrix[,-11]
+#IV.1 feladat 50*10-es mátrix + normálelo. +szóráskritérium
+the_matrix <- matrix(1:50, 50, 11)
+the_matrix[, 1:10] <- (matrix(rnorm(500, mean = 0, sd =
+                                       the_matrix[, 11]), 50, 10))
 
-persp(probamatrix,expand=0.2) #Ezt csak itt hagytam, elég jól néz ki :D
+the_matrix <- the_matrix[, -11]
 
-    #IV.2 feladat sorok szórásának kiszámolása for ciklussal. Eredmény: vektor.
+persp(the_matrix, expand = 0.2) #Ezt csak itt hagytam, elég jól néz ki :D
+
+#IV.2 feladat sorok szórásának kiszámolása for ciklussal. Eredmény: vektor.
 for_sd <- vector("numeric")
 
-for (i in 1:nrow(probamatrix)) {
-for_sd[i] <- sd(probamatrix[i,])  
- }
+for (i in 1:nrow(the_matrix)) {
+  for_sd[i] <- sd(the_matrix[i, ])
+}
 
 #IV.3 feladat: sorok szórásának kiszámolása az apply-al. Eredmény: vektor.
 app_sd <- c()
-app_sd <- apply(probamatrix,1,sd)
+app_sd <- apply(the_matrix, 1, sd)
 
 #IV.4 feladat: Normalizálás + átlagszámítás
+normal_matrix <- apply(the_matrix, 1, be_normal_pls)
+summary(normal_matrix) #Ellenőrzök
 
-normal_matrix <- apply(probamatrix,1,be_normal_pls)
-summary(normal_matrix) #ellenőrzök
-
-apply(normal_matrix,1,mean)
+apply(normal_matrix, 1, mean)
 
 
 ## V. feladat
-  #V.1 feladat: Package + dataset behívása
+#V.1 feladat: Package + dataset behívása
+if (!("fivethirtyeight" %in% installed.packages())){
+install.packages("fivethirtyeight")}
 
+require(fivethirtyeight)
+data(comic_characters)
 
+#V.2 feladat: Névkezelés forciklus nélkül.
+comic_characters$name <- citizen_mode(comic_characters$name)
 
+#V.3 feladat: Get_gender függvény írása
+get_gender ("Loki Laufeyson") #Lokinál most kiadja az NA-kat is.
+get_gender("Katherine Pryde") #Itt csak egyet, ad, de ő egyedül is van.
+get_gender("Thor") #Thornál visszaad 4 male-t. Erre gondoltál?
 
+#Innen néztem még párat.
+rep_heros <- data.frame(table(comic_characters$name))
+rep_heros <- rep_heros[rep_heros$Freq > 1, ]
+
+get_gender("Adolf Hitler")
+get_gender("Hercules")
+get_gender("Adam")
